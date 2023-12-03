79cf860935673e2d5617bff5942f67f92f2d7563
burosalih <sprajt.kola.sveps@gmail.com>
2023-12-03 13:30:46 +0100

    Dodavanje metoda klasi Auto

--- src/include/klase/Auto.h
+++ src/include/klase/Auto.h
@@ -9,15 +9,28 @@ public:
 
     std::string getMarka() const;
     std::string getModel() const;
+    std::string getGorivo() const;
+
+    double getCijena() const;
+
     int getGodina() const;
 
     void setMarka(const std::string& marka);
     void setModel(const std::string& model);
     void setGodina(int godina);
+    void setGorivo(const std::string& gorivo);
+    void setCijena(double cijena);
+
+    void unosAuta();
+    void ispisAuta() const;
+    void izmjenaAuta();
 
 private:
     std::string marka;
     std::string model;
+    std::string gorivo;
+
+    double cijena;
     int godina;
 };
 
