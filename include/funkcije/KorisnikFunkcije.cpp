a8233bf1b53d984868028ef5a3498e5a9f14e313
burosalih <sprajt.kola.sveps@gmail.com>
2023-12-12 20:49:59 +0100

    Dodavanje funkcija za korisnika

---
+++ include/funkcije/KorisnikFunkcije.cpp
@@ -0,0 +1,16 @@
+#include "../include/klase/Korisnik.h"
+#include <iostream>
+
+static Korisnik registerUser() {
+    std::string username, password;
+    std::cout << "Enter a username: ";
+    std::cin >> username;
+    std::cout << "Enter a password: ";
+    std::cin >> password;
+
+    return Korisnik(username, password);
+}
+
+static bool loginUser(const Korisnik& korisnik, const std::string& enteredUsername, const std::string& enteredPassword) {
+    return (korisnik.getUsername() == enteredUsername && korisnik.getPassword() == enteredPassword);
+}
